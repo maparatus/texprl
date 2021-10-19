81657517ecaae9587328c22f417c424c6da3c649
orangemug <info@orangemug.co.uk>
2021-10-19 09:21:48 +0100

    More lint fixes.

--- texprl.js
+++ texprl.js
@@ -1,35 +1,37 @@
-import {EditorState, SelectionRange, Compartment} from "@codemirror/state";
-import {keymap, Decoration} from "@codemirror/view";
-import {basicSetup, EditorView} from "@codemirror/basic-setup"
-import {defaultKeymap} from "@codemirror/commands";
-import {indentMore, indentLess} from "@codemirror/commands";
-import {PluginField, WidgetType, ViewUpdate, ViewPlugin} from "@codemirror/view";
-import {syntaxTree} from "@codemirror/language";
-import {wast, fromArrayAst, toArrayAst} from './parser/index.js';
-import {h, render} from 'preact';
-import { v4 as uuidv4 } from 'uuid';
-import Color from 'color';
-import stringify from 'json-stringify-pretty-compact';
+import { EditorState, SelectionRange, Compartment } from "@codemirror/state";
+import { keymap, Decoration } from "@codemirror/view";
+import { basicSetup, EditorView } from "@codemirror/basic-setup";
+import { defaultKeymap } from "@codemirror/commands";
+import { indentMore, indentLess } from "@codemirror/commands";
+import {
+  PluginField,
+  WidgetType,
+  ViewUpdate,
+  ViewPlugin,
+} from "@codemirror/view";
+import { syntaxTree } from "@codemirror/language";
+import { wast, fromArrayAst, toArrayAst } from "./parser/index.js";
+import { h, render } from "preact";
+import { v4 as uuidv4 } from "uuid";
+import Color from "color";
+import stringify from "json-stringify-pretty-compact";
 import {
   BooleanWidget,
   ColorWidget,
   DateTimeWidget,
   DateWidget,
-  ReferenceWidget
-} from './widgets';
+  ReferenceWidget,
+} from "./widgets";
 
 export {
   BooleanWidget,
   ColorWidget,
   DateTimeWidget,
   DateWidget,
-  ReferenceWidget
+  ReferenceWidget,
 };
 
-export {
-  toArrayAst,
-  fromArrayAst,
-};
+export { toArrayAst, fromArrayAst };
 
 // export {
 //   TexprlLanguageSupport,
@@ -38,68 +40,72 @@ export {
 //   texprl,
 // }
 
-function generatePluginFromWidgets (widgets) {
+function generatePluginFromWidgets(widgets) {
   return null;
 }
 
 function checkboxes(view, texprl) {
-  let widgets = []
-  for (let {from, to} of view.visibleRanges) {
+  let widgets = [];
+  for (let { from, to } of view.visibleRanges) {
     syntaxTree(view.state).iterate({
-      from, to,
+      from,
+      to,
       enter: (type, from, to) => {
         [
           BooleanWidget,
           ColorWidget,
           DateTimeWidget,
           DateWidget,
-          ReferenceWidget
-        ].forEach(widgetClass => {
+          ReferenceWidget,
+        ].forEach((widgetClass) => {
           const widget = widgetClass.treeEnter(view, type, from, to, texprl);
           if (widget) {
             widgets.push(widget);
           }
         });
-      }
-    })
+      },
+    });
   }
   return Decoration.set(widgets);
 }
 
 const checkboxPlugin = (texprl) => {
-  return ViewPlugin.fromClass(class {
-    constructor(view) {
-      this.decorations = checkboxes(view, texprl);
-    }
-
-    update(update) {
-      console.log("this.decorations", this.decorations);
-      this.decorations.chunk.forEach(chunk => {
-        chunk.value.forEach(v => {
-          if (v.widget.update) {
-            v.widget.update(update);
-          }
-        })
-      });
-
-      if (update.docChanged || update.viewportChanged) {
-        this.decorations = checkboxes(update.view, texprl);
+  return ViewPlugin.fromClass(
+    class {
+      constructor(view) {
+        this.decorations = checkboxes(view, texprl);
       }
-    }
-  }, {
-    decorations: (v) => {
-      return v.decorations
+
+      update(update) {
+        console.log("this.decorations", this.decorations);
+        this.decorations.chunk.forEach((chunk) => {
+          chunk.value.forEach((v) => {
+            if (v.widget.update) {
+              v.widget.update(update);
+            }
+          });
+        });
+
+        if (update.docChanged || update.viewportChanged) {
+          this.decorations = checkboxes(update.view, texprl);
+        }
+      }
     },
-    provide: EditorView.updateListener.of(update => {
-      console.log("foobar", update)
-    }),
-  })
-}
+    {
+      decorations: (v) => {
+        return v.decorations;
+      },
+      provide: EditorView.updateListener.of((update) => {
+        console.log("foobar", update);
+      }),
+    }
+  );
+};
 
 export class TexprlEditor {
-  constructor (container, initialDoc, opts={}) {
+  constructor(container, initialDoc, opts = {}) {
     this.container = container;
-    this.element = document.createElement('div');
+    this.element = document.createElement("div");
     this.element.classList.add("texprl");
     this.container.appendChild(this.element);
     this.widgets = [].concat(opts.widgets);
@@ -112,11 +118,11 @@ export class TexprlEditor {
   _onDispatch = (transaction) => {
     this.view.update([transaction]);
     this.onDispatch(this);
-  }
+  };
 
   autoFormat = () => {
     const formattedText = fromArrayAst(toArrayAst(this.view, this), this);
-    console.log("autoFormat", {formattedText});
+    console.log("autoFormat", { formattedText });
     const state = this.view.state;
     this.view.dispatch({
       changes: [
@@ -124,18 +130,20 @@ export class TexprlEditor {
           from: 0,
           to: state.doc.length,
           insert: formattedText,
-        }
-      ]
+        },
+      ],
     });
-  }
+  };
 
   checkLookupFromBackendId = (key) => {
     // TODO: This should be contextual.
     const arr = [];
     console.log(this.lookup);
-    const found = this.lookup.flatMap(i => i.values).find(item => {
-      return item.backendId === key;
-    })
+    const found = this.lookup
+      .flatMap((i) => i.values)
+      .find((item) => {
+        return item.backendId === key;
+      });
     return found;
   };
 
@@ -143,24 +151,28 @@ export class TexprlEditor {
     // TODO: This should be contextual.
     const arr = [];
     console.log(this.lookup);
-    const found = this.lookup.flatMap(i => i.values).find(item => {
-      return item.editorId === key;
-    })
+    const found = this.lookup
+      .flatMap((i) => i.values)
+      .find((item) => {
+        return item.editorId === key;
+      });
     return found;
   };
 
-  _setupCodemirror (initialDoc) {
-    const language = new Compartment;
-    const tabSize = new Compartment;
+  _setupCodemirror(initialDoc) {
+    const language = new Compartment();
+    const tabSize = new Compartment();
 
     let startState = EditorState.create({
-      doc: initialDoc || '',
+      doc: initialDoc || "",
       extensions: [
         checkboxPlugin(this),
         basicSetup,
-        language.of(wast(() => {
-          return this.lookup.flatMap(i => i.values);
-        })),
+        language.of(
+          wast(() => {
+            return this.lookup.flatMap((i) => i.values);
+          })
+        ),
         keymap.of([
           ...defaultKeymap,
           {
@@ -175,7 +187,7 @@ export class TexprlEditor {
           },
         ]),
       ],
-    })
+    });
 
     this.view = new EditorView({
       state: startState,
@@ -184,7 +196,7 @@ export class TexprlEditor {
     });
   }
 
-  setLookup (lookup) {
+  setLookup(lookup) {
     this.lookup = [].concat(lookup);
   }
 }
